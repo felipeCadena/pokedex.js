e04e920f9a68c3665705528bb929018752664881
felipeCadena <felipecaden@gmail.com>
2023-06-29 21:23:35 -0300

    correcoes

--- src/main.js
+++ src/main.js
@@ -2,12 +2,8 @@ import './style.css';
 
 const inputNameId = document.querySelector('.input');
 const img = document.querySelector('.pokemon');
-// const random = document.querySelector('.random');
 const idPokemon = document.querySelector('.id');
 const namePokemon = document.querySelector('.name');
-const nextPokemon = document.querySelector('.next');
-// const prevPokemon = document.querySelector('.prev');
-// const id = document.querySelector('.id');
 
 async function fetchPokemon(pokemon) {
   const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -16,20 +12,16 @@ async function fetchPokemon(pokemon) {
 }
 
 const pokemonImage = async (pokemon) => {
-  const data = await fetchPokemon(pokemon.toLowerCase());
+  const { id, name, sprites } = await fetchPokemon(pokemon);
 
-  idPokemon.innerHTML = `${data.id} -`;
-  namePokemon.innerHTML = data.name.toUpperCase();
-  img.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+  idPokemon.innerHTML = `${id} -`;
+  namePokemon.innerHTML = name.toUpperCase();
+  img.src = sprites.versions['generation-v']['black-white'].animated.front_default;
 };
 
 inputNameId.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     event.preventDefault();
-    pokemonImage(inputNameId.value);
+    pokemonImage(inputNameId.value.toLowerCase());
   }
-});
-
-nextPokemon.addEventListener('click', () => {
-
 });
